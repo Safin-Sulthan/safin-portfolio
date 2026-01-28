811ac25b978ce63caa89dcbfa402fb01645e9b27
safinsulthan07 <nsafinsulthan@gmail.com>
2026-01-28 16:53:23 +0530

    Updated in Main.jsx to avoid routing issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { HashRouter} from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from './App.jsx';
 import './index.css';
 createRoot(document.getElementById('root')).render(
-  <HashRouter basename="/safin-portfolio">
+  <HashRouter>
     <App />
   </HashRouter>,
 );
